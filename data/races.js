89c80146a367f27ba8a1ba0a301d39bcf7839eb6
sidneyfears <sidneyfears@googlemail.com>
2026-08-29 17:33:02 +0200

    Add race calendar data

---
+++ data/races.js
@@ -0,0 +1,7 @@
+const races = [
+  { date: '2026-09-11', name: 'SportScheck RUN Köln 2026', distance: '10 km', status: 'Geplant' },
+  { date: '2026-09-27', name: 'Bickendorfer Büdchenlauf 2026', distance: '10 km', status: 'Geplant' },
+  { date: '2026-10-04', name: 'Generali Halbmarathon Köln 2026', distance: 'Halbmarathon', status: 'Hauptziel' },
+  { date: '2027-03-21', name: 'Közon21', distance: 'Halbmarathon', status: 'Geplant' },
+  { date: '2027-05-09', name: 'Wings for Life Run 2027', distance: 'App Run', status: 'Geplant' }
+];
